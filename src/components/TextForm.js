82abfa6554ead545c264253a16a3a6dc182722d2
swapnil <swapnilvyas18@gmail.com>
2022-01-22 03:11:37 +0530

    Video 11 Completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,6 +10,19 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText)
   }
+  const handleClearClick = ()=>{
+    let newText = '';
+    setText(newText)
+  }
+  const handleCopyClick = ()=>{
+    let text = document.getElementById('myBox');
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleExtraSpaces = ()=>{
+    let newText = text.split(/[ ]+/); /*Js Rejects*/
+    setText(newText.join(" "))
+  }
   const handleOnChange = (event)=>{
     setText(event.target.value)
   }
@@ -23,6 +36,9 @@ export default function TextForm(props) {
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       </div>
       <div className="container my-3">
         <h2>Your text summary</h2>
