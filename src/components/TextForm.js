def296b621d47f2c79f611b2a0e9040ea49e996b
swapnil <swapnilvyas18@gmail.com>
2022-01-22 04:33:51 +0530

    Video 13 Completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,23 +5,28 @@ export default function TextForm(props) {
   const handleUpClick = ()=>{
     let newText = text.toUpperCase();
     setText(newText)
+    props.showAlert('Converted to uppercase!', 'success');
   }
   const handleLoClick = ()=>{
     let newText = text.toLowerCase();
     setText(newText)
+    props.showAlert('Converted to lowercase!', 'success');
   }
   const handleClearClick = ()=>{
     let newText = '';
     setText(newText)
+    props.showAlert('Text Cleared!', 'success');
   }
   const handleCopyClick = ()=>{
     let text = document.getElementById('myBox');
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert('Copied to Clipboard!', 'success');
   }
   const handleExtraSpaces = ()=>{
     let newText = text.split(/[ ]+/); /*Js Rejects*/
     setText(newText.join(" "))
+    props.showAlert('Extra spaces removed!', 'success');
   }
   const handleOnChange = (event)=>{
     setText(event.target.value)
